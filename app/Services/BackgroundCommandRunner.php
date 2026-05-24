e57fc1f7858f2bd4bf87bd4bd5d33967e452cb8f
kuzya3d. <[email protected]>
2026-05-25 01:13:43 +0500

    fix linux background runner

--- app/Services/BackgroundCommandRunner.php
+++ app/Services/BackgroundCommandRunner.php
@@ -8,12 +8,29 @@ class BackgroundCommandRunner
     {
         $php = PHP_BINARY;
         $artisan = base_path('artisan');
-        $fullCommand = sprintf(
-            'start /B "" %s %s %s',
-            escapeshellarg($php),
-            escapeshellarg($artisan),
-            $command
-        );
+
+        if (PHP_OS_FAMILY === 'Windows') {
+            $fullCommand = sprintf(
+                'start /B "" %s %s %s',
+                escapeshellarg($php),
+                escapeshellarg($artisan),
+                $command
+            );
+        } else {
+            $log = storage_path('logs/background-artisan.log');
+
+            if (! is_dir(dirname($log))) {
+                @mkdir(dirname($log), 0775, true);
+            }
+
+            $fullCommand = sprintf(
+                'nohup %s %s %s >> %s 2>&1 &',
+                escapeshellarg($php),
+                escapeshellarg($artisan),
+                $command,
+                escapeshellarg($log)
+            );
+        }
 
         $handle = @popen($fullCommand, 'r');
 
